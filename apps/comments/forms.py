0900135a51d123e012dd9d2632aef790d971dbd2
myTeemo <mingyang.hey@gmail.com>
2017-06-30 14:15:46 +0800

    配置留言的表单

--- apps/comments/forms.py
+++ apps/comments/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 
 from .models import Comment
+from .models import Contact
 
 __author__ = "Eilene HE"
 __date__ = '2017/6/26 17:47'
@@ -12,4 +13,9 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['name', 'email', 'url', 'text']
 
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'subject', 'text']
 
