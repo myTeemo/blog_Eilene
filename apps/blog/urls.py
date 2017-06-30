76656ae73f9651946b061da8ae9ca666c0ebc3f7
myTeemo <mingyang.hey@gmail.com>
2017-06-30 14:13:29 +0800

    配置留言和关于自己的url

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -6,6 +6,8 @@ from .views import ArticleDetailView
 from .views import IndexView
 from .views import ArchivesView
 from .views import CategoryView
+from .views import AboutView
+from .views import ContactView
 
 __author__ = "Eilene HE"
 __date__ = '2017/6/26 10:29'
@@ -16,4 +18,6 @@ urlpatterns = [
     url(r'^article_detail/(?P<pk>[0-9]+)/$', ArticleDetailView.as_view(), name="article_detail"),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', ArchivesView.as_view(), name='archives'),
     url(r'^category/(?P<pk>[0-9]+)/$', CategoryView.as_view(), name='category'),
+    url(r'^about/$', AboutView.as_view(), name='about'),
+    url(r'^contact/$', ContactView.as_view(), name='contact'),
 ]
