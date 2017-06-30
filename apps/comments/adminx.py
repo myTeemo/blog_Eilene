ac39697b8c35436351ed0a5eeb593ee800dfaeac
myTeemo <mingyang.hey@gmail.com>
2017-06-30 14:15:05 +0800

    添加留言的页面配置

--- apps/comments/adminx.py
+++ apps/comments/adminx.py
@@ -2,6 +2,7 @@
 import xadmin
 
 from .models import Comment
+from .models import Contact
 
 __author__ = "Eilene HE"
 __date__ = '2017/6/26 17:40'
@@ -13,4 +14,11 @@ class CommentsAdmin(object):
     list_filter = ['name', 'email', 'url', 'text', 'created_time', 'article']
 
 
+class ContactAdmin(object):
+    list_display = ['name', 'email', 'subject', 'created_time', 'text']
+    search_fields = ['name', 'email', 'subject', 'text']
+    list_filter = ['name', 'email', 'subject', 'text']
+
+
 xadmin.site.register(Comment, CommentsAdmin)
+xadmin.site.register(Contact, ContactAdmin)
