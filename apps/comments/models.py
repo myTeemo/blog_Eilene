ad1d3d138e230df4cc8adba28e86dd8311966cf6
myTeemo <mingyang.hey@gmail.com>
2017-06-30 14:16:47 +0800

    添加留言的模型

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -23,3 +23,19 @@ class Comment(models.Model):
     def __unicode__(self):
         return self.text[:20]
 
+
+class Contact(models.Model):
+    name = models.CharField(verbose_name=u'姓名', max_length=30)
+    email = models.EmailField(verbose_name=u'电子邮件', max_length=255)
+    subject = models.CharField(verbose_name=u'主题', max_length=20)
+    text = models.TextField(verbose_name=u'留言信息')
+
+    created_time = models.DateTimeField(verbose_name=u'留言时间', default=datetime.now)
+
+    def __unicode__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = u'留言'
+        verbose_name_plural = verbose_name
+
