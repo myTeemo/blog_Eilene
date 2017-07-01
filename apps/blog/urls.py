7bce6d2aef98444ab172bdf4d256335c1f4e03f5
myTeemo <mingyang.hey@gmail.com>
2017-07-02 00:40:42 +0800

    配置media的目录，配置image的url

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 from .views import ArticleDetailView
@@ -8,6 +10,8 @@ from .views import ArchivesView
 from .views import CategoryView
 from .views import AboutView
 from .views import ContactView
+from .views import ImageLibView
+
 
 __author__ = "Eilene HE"
 __date__ = '2017/6/26 10:29'
@@ -20,4 +24,7 @@ urlpatterns = [
     url(r'^category/(?P<pk>[0-9]+)/$', CategoryView.as_view(), name='category'),
     url(r'^about/$', AboutView.as_view(), name='about'),
     url(r'^contact/$', ContactView.as_view(), name='contact'),
+    url(r'^images/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<image_address>.*)/$', ImageLibView.as_view(), name='image'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
