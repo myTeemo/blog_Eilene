ab892f8b0de435eb64d1e376567e1e37cc0da469
myTeemo <mingyang.hey@gmail.com>
2017-07-02 13:22:46 +0800

    添加图片库的管理类

--- apps/blog/adminx.py
+++ apps/blog/adminx.py
@@ -5,6 +5,7 @@ from xadmin import views
 from .models import Category
 from .models import Tag
 from .models import Article
+from .models import ImageLib
 
 __author__ = "Eilene HE"
 __date__ = '2017/6/26 10:08'
@@ -28,6 +29,13 @@ class ArticleAdmin(object):
     list_filter = ['title', 'body', 'created_time', 'modified_time', 'excerpt', 'category', 'tags', 'author']
 
 
+class ImageLibAdmin(object):
+    list_display = ['name', 'image', 'article', 'created_time']
+    search_fields = ['name', 'article']
+    list_filter = ['name', 'article', 'image']
+
+
 xadmin.site.register(Category, CategoryAdmin)
 xadmin.site.register(Tag, TagAdmin)
 xadmin.site.register(Article, ArticleAdmin)
+xadmin.site.register(ImageLib, ImageLibAdmin)
