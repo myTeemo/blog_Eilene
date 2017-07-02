5e00095866a1eb43889a3e4bdb0ff4942049d2db
myTeemo <mingyang.hey@gmail.com>
2017-07-02 13:25:30 +0800

    修改提交表单的url字段是必须的，否则在提交空表单的时候，会导致页面布局紊乱

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -10,7 +10,7 @@ from blog.models import Article
 class Comment(models.Model):
     name = models.CharField(verbose_name=u'姓名', max_length=30)
     email = models.EmailField(verbose_name=u'电子邮箱', max_length=255)
-    url = models.URLField(verbose_name=u'站点', blank=True)
+    url = models.URLField(verbose_name=u'站点')
     text = models.TextField(verbose_name=u'评论内容')
     created_time = models.DateTimeField(verbose_name=u'评论时间', default=datetime.now)
 
